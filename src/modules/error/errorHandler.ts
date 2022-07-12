3e35ec99f50ffa008008c95447e762a190159a30
May <57610348+RyanD524@users.noreply.github.com>
2022-07-11 23:09:54 -0400

    better alert system

--- src/modules/error/errorHandler.ts
+++ src/modules/error/errorHandler.ts
@@ -1,10 +1,37 @@
+enum codes {
+    ALERT_INFO,
+    ALERT_GENERAL,
+    ALERT_SUCCESS,
+    ALERT_WARNING,
+    ALERT_FAILURE,
+};
+
+
+
 let alertSpace = document.getElementById("alertOverlay")!;
 let alerts: HTMLElement[] = [];
 
-let alert = (input: string, isError:boolean=false) => {
+let alert = (input: string, code:codes=codes.ALERT_SUCCESS) => {
     let aDiv = document.createElement('div');
 
-    aDiv.classList.add(isError ? "error" : "success");
+    switch (code) {
+        case codes.ALERT_SUCCESS:
+            aDiv.classList.add("success");
+            break;
+        case codes.ALERT_GENERAL:
+            aDiv.classList.add("general");
+            break;
+        case codes.ALERT_INFO:
+            aDiv.classList.add("info");
+            break;
+        case codes.ALERT_WARNING:
+            aDiv.classList.add("warning");
+            break;
+        case codes.ALERT_FAILURE:
+            aDiv.classList.add("errror");
+            break;
+    }
+
     aDiv.classList.add("overlayBlob");
     aDiv.innerText = input;
     setTimeout(() => {
